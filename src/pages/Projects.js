5c46da8b13b2fbb600d7abcf0753f7539b71e4af
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-02 15:50:19 -0400

    updating

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -26,7 +26,7 @@ function Projects(props) {
     return projects.map((project) => (
       <div>
         <h1>{project.name}</h1>
-        <img src={project.image} />
+        <img src={project.image} alt="description" />
         <a href={project.git}>
           <button>Github</button>
         </a>
