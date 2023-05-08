6752a8751caa7468f2ee900e8ce29f96016f3e76
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-08 14:41:15 -0400

    updating photo

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ export default function About(props) {
 
 		// make api call and get response
     const response = await fetch("./about.json");
-
+console.log(response)
 		// turn response into javascript object
     const data = await response.json();
 
@@ -18,11 +18,12 @@ export default function About(props) {
   };
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => getAboutData(), []);
+  useEffect(() => {getAboutData()}, []);
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => (
     <div>
+      <img src= {about.headshot}/>
       <h2>{about.name}</h2>
       <h3>{about.email}</h3>
       <p>{about.bio}</p>
